918873c74d62131b0337c3b3cc409cec3c798ee8
timjini <hatim.jini@gmail.com>
2022-11-07 08:37:44 +0300

    updatE

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,11 +1,19 @@
-export const API_ROOT = 'http://localhost:3001/api/v1'; 'https://aid-app.netlify.app/api/v1';
-export const API_WS_ROOT = 'ws://localhost:3000/cable'; 'https://aid-app.netlify.app/cable';
-export const HEADERS = {
+ export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
 };
-export const API_REQUESTS = 'http://localhost:3001/api/v1/requests'; 'https://aid-app.netlify.app/api/v1/requests';
-export const API_FULFILLMENTS = 'http://localhost:3001/api/v1/fulfillments'; 'https://aid-app.netlify.app/api/v1/fulfillments';
-export const API_DOCUMENTS = 'http://localhost:3001/api/v1/documents'; 'https://aid-app.netlify.app/api/v1/documents';
-export const API_USERS = 'http://localhost:3001/api/v1/users'; 'https://aid-app.netlify.app/api/v1/users';
-export const API_TWEETS = 'http://localhost:3001/api/v1/tweets';  'https://aid-app.netlify.app/api/v1/tweets';
+export const API_REQUESTS = 'http://localhost:3001/api/v1/requests'; 
+export const API_FULFILLMENTS = 'http://localhost:3001/api/v1/fulfillments'; 
+export const API_DOCUMENTS = 'http://localhost:3001/api/v1/documents'; 
+export const API_USERS = 'http://localhost:3001/api/v1/users'; 
+export const API_TWEETS = 'http://localhost:3001/api/v1/tweets';  
+export const API_ROOT = 'http://localhost:3001/api/v1'; 
+export const API_WS_ROOT = 'ws://localhost:3000/cable'; 
+
+// export const API_REQUESTS = 'http://localhost:3001/api/v1/requests'; 
+// export const API_FULFILLMENTS = 'http://localhost:3001/api/v1/fulfillments'; 
+// export const API_DOCUMENTS = 'http://localhost:3001/api/v1/documents'; 
+// export const API_USERS = 'http://localhost:3001/api/v1/users'; 
+// export const API_TWEETS = 'http://localhost:3001/api/v1/tweets';  
+// export const API_ROOT = 'http://localhost:3001/api/v1'; 
+// export const API_WS_ROOT = 'ws://localhost:3000/cable'; 
