29bc28fefd0f6008db7edf41e15ddd0480caef56
timjini <hatim.jini@gmail.com>
2022-11-07 10:48:33 +0300

    new url

--- src/constant/index.js
+++ src/constant/index.js
@@ -10,10 +10,10 @@
 // export const API_ROOT = 'http://localhost:3001/api/v1'; 
 // export const API_WS_ROOT = 'ws://localhost:3000/cable'; 
 
-export const API_REQUESTS = 'https://hidden-eyrie-18402.herokuapp.com/api/v1/requests'; 
-export const API_FULFILLMENTS = 'https://hidden-eyrie-18402.herokuapp.com/api/v1/fulfillments'; 
-export const API_DOCUMENTS = 'https://hidden-eyrie-18402.herokuapp.com/api/v1/documents'; 
-export const API_USERS = 'https://hidden-eyrie-18402.herokuapp.com/api/v1/users'; 
-export const API_TWEETS = 'https://hidden-eyrie-18402.herokuapp.com/api/v1/rooms/1/tweets';  
-export const API_ROOT = 'https://hidden-eyrie-18402.herokuapp.com/api/v1'; 
-export const API_WS_ROOT = 'https://hidden-eyrie-18402.herokuapp.com/cable'; 
+export const API_REQUESTS = 'https://aid--backend.herokuapp.com//api/v1/requests'; 
+export const API_FULFILLMENTS = 'https://aid--backend.herokuapp.com//api/v1/fulfillments'; 
+export const API_DOCUMENTS = 'https://aid--backend.herokuapp.com//api/v1/documents'; 
+export const API_USERS = 'https://aid--backend.herokuapp.com//api/v1/users'; 
+export const API_TWEETS = 'https://aid--backend.herokuapp.com//api/v1/rooms/1/tweets';  
+export const API_ROOT = 'https://aid--backend.herokuapp.com//api/v1'; 
+export const API_WS_ROOT = 'https://aid--backend.herokuapp.com//cable'; 
