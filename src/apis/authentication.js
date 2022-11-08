914425ef8fa3fbeeef01da5e19ac94d34a06459a
timjini <hatim.jini@gmail.com>
2022-11-08 04:16:47 +0300

    links

--- src/apis/authentication.js
+++ src/apis/authentication.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/';
-// const baseUrl = 'https://aid--backend.herokuapp.com/';
+// const baseUrl = 'http://localhost:3001/';
+const baseUrl = 'https://aid--backend.herokuapp.com/';
 
 const login = payload => axios.post(`${baseUrl}api/v1/login`, payload);
 
--- src/constant/index.js
+++ src/constant/index.js
@@ -2,18 +2,18 @@
   'Content-Type': 'application/json',
   Accept: 'application/json',
 };
-export const API_REQUESTS = 'http://localhost:3001/api/v1/requests'; 
-export const API_FULFILLMENTS = 'http://localhost:3001/api/v1/fulfillments'; 
-export const API_DOCUMENTS = 'http://localhost:3001/api/v1/documents'; 
-export const API_USERS = 'http://localhost:3001/api/v1/users'; 
-export const API_TWEETS = 'http://localhost:3001/api/v1/rooms/1/tweets';  
-export const API_ROOT = 'http://localhost:3001/api/v1'; 
-export const API_WS_ROOT = 'ws://localhost:3001/cable'; 
+// export const API_REQUESTS = 'http://localhost:3001/api/v1/requests'; 
+// export const API_FULFILLMENTS = 'http://localhost:3001/api/v1/fulfillments'; 
+// export const API_DOCUMENTS = 'http://localhost:3001/api/v1/documents'; 
+// export const API_USERS = 'http://localhost:3001/api/v1/users'; 
+// export const API_TWEETS = 'http://localhost:3001/api/v1/rooms/1/tweets';  
+// export const API_ROOT = 'http://localhost:3001/api/v1'; 
+// export const API_WS_ROOT = 'ws://localhost:3001/cable'; 
 
-// export const API_REQUESTS = 'https://aid--backend.herokuapp.com/api/v1/requests'; 
-// export const API_FULFILLMENTS = 'https://aid--backend.herokuapp.com/api/v1/fulfillments'; 
-// export const API_DOCUMENTS = 'https://aid--backend.herokuapp.com/api/v1/documents'; 
-// export const API_USERS = 'https://aid--backend.herokuapp.com/api/v1/users'; 
-// export const API_TWEETS = 'https://aid--backend.herokuapp.com/api/v1/rooms/1/tweets';  
-// export const API_ROOT = 'https://aid--backend.herokuapp.com/api/v1'; 
-// export const API_WS_ROOT = 'https://aid--backend.herokuapp.com/cable'; 
+export const API_REQUESTS = 'https://aid--backend.herokuapp.com/api/v1/requests'; 
+export const API_FULFILLMENTS = 'https://aid--backend.herokuapp.com/api/v1/fulfillments'; 
+export const API_DOCUMENTS = 'https://aid--backend.herokuapp.com/api/v1/documents'; 
+export const API_USERS = 'https://aid--backend.herokuapp.com/api/v1/users'; 
+export const API_TWEETS = 'https://aid--backend.herokuapp.com/api/v1/rooms/1/tweets';  
+export const API_ROOT = 'https://aid--backend.herokuapp.com/api/v1'; 
+export const API_WS_ROOT = 'https://aid--backend.herokuapp.com/cable'; 
