d05671c5a0048c584972f90801328db0be0c9350
timjini <hatim.jini@gmail.com>
2022-12-08 05:11:25 +0300

    local storage

--- src/components/Dashboard/LocateUser.js
+++ src/components/Dashboard/LocateUser.js
@@ -19,6 +19,7 @@ const LocateUser = () => {
            map.panTo(e.latlng, map.setView(e.latlng))
            //save location to user session 
             sessionStorage.setItem('userLocation', JSON.stringify(e.latlng));
+            localStorage.setItem('userLocation', JSON.stringify(e.latlng));
             map.loading = false;
             setLoading(false);
           },
